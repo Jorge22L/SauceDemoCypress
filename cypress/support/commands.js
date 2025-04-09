ea22cbfe8ffa8e687359b92da4768ca2f6513a79
Jorge Morales R <jorgeluismreyes@gmail.com>
2025-04-08 20:17:52 -0600

    Adding custom login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,13 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('login', (url, email, password) => {
+    console.log("Login Params:  ", {url, email, password});
+    cy.visit(url);
+    cy.get('#user-name').type(email);
+    cy.get('#password').type(password);   
+    cy.get('#login-button').click(); 
+})
+
+Cypress.Commands.add('logout', () => {
+    cy.get('#react-burger-menu-btn').click();
+    cy.get('[data-test="logout-sidebar-link"]').click();
+    cy.url().should('eq', 'https://www.saucedemo.com/')
+})
